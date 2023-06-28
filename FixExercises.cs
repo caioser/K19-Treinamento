1509ea4f9660308140669afaec9d1c845496c2cd
Caio Sergio <csrmyo@gmail.com>
2023-06-28 17:08:30 -0300

    Adicionar arquivos de projeto.

---
+++ FixExercises.cs
@@ -0,0 +1,92 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+namespace K19_Treinamento
+{
+    internal class FixExercises
+    {
+        public static void MyName100Times()
+        { 
+            for (int i = 1; i <= 100; i++)
+            {
+                Console.WriteLine($"Caio Sergio {i}");
+            }
+        }
+
+        public static void OneTo100()
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        public static void PrintPattern1()
+        {
+            for (int contador = 1; contador <= 100; contador++)
+            {
+                int resto = contador % 2;
+                if (resto == 1)
+                {
+                    Console.WriteLine("*");
+                }
+                else
+                {
+                    Console.WriteLine("**");
+                }
+            }
+        }
+
+        public static void PrintPattern2()
+        {
+            for (int contador = 1; contador <= 100; contador++)
+            {
+                int resto = contador % 4;
+                if (resto == 0)
+                {
+                    Console.WriteLine("PI");
+                }
+                else
+                {
+                    Console.WriteLine(contador);
+                }
+            }
+        }
+
+        public static void StarTri(int i=5)
+        {
+            for (int contador = 1; contador <= i; contador++)
+            {
+                string Star = new('*', contador);
+                Console.WriteLine(Star);
+            }
+        }
+
+        public static void StarTriMany(int i=3)
+        {
+            for (int contador = 1; contador <= i; contador++)
+            {
+                StarTri();
+            }
+        }
+
+        public static void Fibonacci(int until=30)
+        {
+            // usar Collections ou Arrays, do jeito que está printa mais que 30 numeros dessa sequencia
+            int First = 0;
+            int Second = 1;
+            int Next = First + Second;
+            Console.Write($"{First}, {Second}, {Next}, ");
+            for (int contador = 0; contador <= until; contador++)
+            {
+                First = Second;
+                Second = Next;
+                Next = First + Second;
+                Console.Write($"{Next}, ");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using K19_Treinamento;
+
+Console.WriteLine("Hello, World!");
+/*
+Método "Main" principal entrypoint e pode ser:
+static void Main()
+static int Main()
+static void Main(string[] args) // agrupa em array de strings cada argumento passado em linha de comando
+static int Main(string[] args)
+*/
+
+// Instruções de nível superior: método "Main" não ímplícito
+if (args.Length > 0) // "args" disponível
+{
+    foreach (var arg in args)
+    {
+        Console.WriteLine($"Argument={arg}");
+    }
+}
+else
+{
+    Console.WriteLine("No arguments");
+}
+
+
+//FixExercises.MyName100Times();
+//FixExercises.OneTo100();
+FixExercises.Fibonacci();
